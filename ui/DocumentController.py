e01c3e02568a21b543bdb218170ffb37ab58e116
sdouglas <shawn.douglas@gmail.com>
2011-01-23 15:22:25 -0500

    Moved main toolbar functions into DocumentController. Updated main_sd to reflect usage of DocWindow.

--- ui/DocumentController.py
+++ ui/DocumentController.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
 # The MIT License
 #
 # Copyright (c) 2011 Wyss Institute at Harvard University
@@ -25,102 +22,81 @@
 #
 # http://www.opensource.org/licenses/mit-license.php
 
-"""
-main.py
-
-Created by Shawn Douglas on 2010-09-26.
-Copyright (c) 2010 . All rights reserved.
-"""
-
-import sys
-import math
-
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-from ui import Styles, SliceHelixGroup, ui_mainwindow
-from data import json_io
+import SliceHelixGroup
 
 
-class CadnanoMainWindow(QMainWindow, ui_mainwindow.Ui_MainWindow):
-    def __init__(self, parent=None):
-        super(CadnanoMainWindow, self).__init__(parent)
-        self.setupUi(self)
+class DocumentController():
+    """
+    Manages interactions between the Document widgets/UI and the Document 
+    model.
+    """
 
-        self.slicescene = QGraphicsScene()
-        self.sliceGraphicsView.setScene(self.slicescene)
-        self.treeModel = TreeModel()
-        self.parts = {}
-        self.assemblies = {}
-
-        # self.pathscene = PathScene(self)
-        # self.pathGraphicsView.setScene(self.pathscene)
-        self.connect(self.actionNewHoneycombPart,\
+    # We store mainWindow because a controller's got to have
+    # references to both the layer above (UI) and the layer below (model)
+    def __init__(self, win):
+        self.mainWindow = win
+        # Connect actions to slots
+        win.connect(win.actionNewHoneycombPart,\
                      SIGNAL("triggered()"),\
                      self.honeycombClicked)
-        self.connect(self.actionNewSquarePart,\
+        win.connect(win.actionNewSquarePart,\
                      SIGNAL("triggered()"),\
                      self.squareClicked)
-
-        self.connect(self.actionNew,\
+        win.connect(win.actionNew,\
                      SIGNAL("triggered()"),\
                      self.newClicked)
-        self.connect(self.actionOpen,\
+        win.connect(win.actionOpen,\
                      SIGNAL("triggered()"),\
                      self.openClicked)
-        self.connect(self.actionClose,\
+        win.connect(win.actionClose,\
                      SIGNAL("triggered()"),\
                      self.closeClicked)
-        self.connect(self.actionSave,\
+        win.connect(win.actionSave,\
                      SIGNAL("triggered()"),\
                      self.saveClicked)
-        self.connect(self.actionSVG,\
+        win.connect(win.actionSVG,\
                      SIGNAL("triggered()"),\
                      self.svgClicked)
-    # end def
 
     def newClicked(self):
-        """docstring for honeycombClicked"""
+        """docstring for newClicked"""
         print "new clicked"
     # end def
 
     def openClicked(self):
-        """docstring for honeycombClicked"""
+        """docstring for openClicked"""
         print "open clicked"
         # self.parts, self.assemblies = self.parts json_io.load(self.treeModel)
     # end def
 
     def closeClicked(self):
-        """docstring for honeycombClicked"""
+        """docstring for closeClicked"""
         print "close clicked"
     # end def
 
     def saveClicked(self):
-        """docstring for honeycombClicked"""
+        """docstring for saveClicked"""
         print "save clicked"
     # end def
 
     def svgClicked(self):
-        """docstring for honeycombClicked"""
+        """docstring for svgClicked"""
         print "svg clicked"
     # end def
 
     def honeycombClicked(self):
         """docstring for honeycombClicked"""
+        print "+honeycomb clicked"
         self.addHoneycombHelixGroup()
+    # end def
 
     def squareClicked(self):
         """docstring for squareClicked"""
+        print "+square clicked"
+    # end def
 
     def addHoneycombHelixGroup(self, nrows=20, ncolumns=20):
         hc = SliceHelixGroup.SliceHelixGroup(nrows, ncolumns, "honeycomb")
-        self.slicescene.addItem(hc)
-
-
-def main():
-    app = QApplication(sys.argv)
-    window = CadnanoMainWindow()
-    window.show()
-    app.exec_()
-
-if __name__ == '__main__':
-    main()
+        self.mainWindow.slicescene.addItem(hc)
