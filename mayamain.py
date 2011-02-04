9bc78ebcb2176fa169a1d366349c115af36d3a61
grinner <a.grinner@gmail.com>
2011-02-04 13:45:39 -0500

    Finished so cadnano can be reloaded without restarting maya after source changes occur

--- mayamain.py
+++ mayamain.py
@@ -37,8 +37,53 @@ if pathadder not in sys.path:
     sys.path.append(pathadder)
 # end if
 
+def getDefaultModules():
+    """
+    gets a list modules that are 
+    installed Maya or Qt modules
+    The list was compiled through printing and additional trial
+    and error to add some additional modules at the bottom of 
+    the list
+    
+    Could do this with sets, but I failed the first try so forget it
+    """
+    f = open(pathadder+'/mayadefaultmodules.txt','r')
+    defaultmodules = {}
+    for line in f:
+        key = line.rstrip('\n')
+        defaultmodules[key] = True
+    #end for
+    f.close()
+    del(f)
+    return defaultmodules
+# end def
+
+def removeModules(moddict):
+    """
+    removes modules related to the program but not 
+    installed Maya or Qt modules
+    """
+    for key in sys.modules.keys():
+        if not moddict.has_key(key):
+            try:
+                del(sys.modules[key])
+                print "module %s deleted" % key
+            except:
+                print "module %s doesn't exist" % key
+        # end if
+    # end for
+# end def
+
+
+"""
+removes a list of modules created for this program
+This allows reloading the program in Maya without having to restart
+"""
+defmods = getDefaultModules()
+removeModules(defmods)
+
+# begin program
 import cadnanomaya
-reload(sys.modules['cadnanomaya'])
 caDNAno = cadnanomaya.caDNAno
 
 #import pymel.core as pm
