857cce26d3a17c514358aefa51896e8357106077
grinner <a.grinner@gmail.com>
2011-06-29 13:54:20 -0400

    began adding the installer for windows

---
+++ installer/win/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup  
+import py2exe  
+import sys
+
+# go one directory above the cadnano2 directory to get it on the path
+sys.path.insert(0, '../../../')
+  
+setup(name="liftr",  
+      version="1.5",  
+      author="Nick Conway",  
+      author_email="nick.conway@wyss.harvard.edu",  
+      #url="",  
+      license="MIT License",  
+      packages=['cadnano2'],  
+      package_data={"cadnano2": ["ui/images/*"]},  
+      windows=['main.py'],  
+      options={"py2exe": {"skip_archive": True, "includes": ["sip"]}})
