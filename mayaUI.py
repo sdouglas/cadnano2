19ca5e8fe0d7a097e42c47f85ca721b72c89406a
leemi <michael.lee@autodesk.com>
2011-07-26 16:37:17 -0400

    small tweak to previous commit

--- mayaUI.py
+++ mayaUI.py
@@ -6,29 +6,31 @@ elementsToHide = ["toolBar", "dockControl"]
 
 mainMenuBarVisible = None
 
+
 def simplifyUI():
     global hiddenElements
     global elementsToHide
-    for i in cmds.lsUI( ctl = True):
+    for i in cmds.lsUI(ctl=True):
         for e in elementsToHide:
-            if i.find(e) != -1 and cmds.control( i, q=True, visible=True):
+            if i.find(e) != -1 and cmds.control(i, q=True, visible=True):
                 hiddenElements.append(i)
                 #print "hiding... " + i
-                cmds.control( i, e=True, visible=False)
+                cmds.control(i, e=True, visible=False)
                 break
 
     global mainMenuBarVisible
-    mainMenuBarVisible = cmds.optionVar( q = "mainWindowMenubarVis")
+    mainMenuBarVisible = cmds.optionVar(q="mainWindowMenubarVis")
     mel.eval("setMainMenubarVisible 0;")
     #mel.eval("toggleModelEditorBarsInAllPanels 0;")
 
-def restoreUI() :
+
+def restoreUI():
     global hiddenElements
     for e in hiddenElements:
         #print "restoring... " + e
-        cmds.control( e, e=True, visible=True)
+        cmds.control(e, e=True, visible=True)
     hiddenElements = []
-    
+
     global mainMenuBarVisible
     mel.eval("setMainMenubarVisible " + str(mainMenuBarVisible) + ";")
     #mel.eval("toggleModelEditorBarsInAllPanels 1;")
