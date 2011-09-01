45149086af032865b80a53a9917fd365028af09b
sdouglas <shawn.douglas@gmail.com>
2011-09-01 13:38:29 -0400

    Adding single-base geometry.

--- views/pathview/normalstrandgraphicsitem.py
+++ views/pathview/normalstrandgraphicsitem.py
@@ -34,24 +34,40 @@ ppL5 = QPainterPath()  # Left 5' PainterPath
 ppR5 = QPainterPath()  # Right 5' PainterPath
 ppL3 = QPainterPath()  # Left 3' PainterPath
 ppR3 = QPainterPath()  # Right 3' PainterPath
+pp53 = QPainterPath()  # Left 5', Right 3' PainterPath
+pp35 = QPainterPath()  # Left 5', Right 3' PainterPath
+
 # set up ppL5 (left 5' blue square)
-ppL5.addRect(0.25 * baseWidth, 0.125 * baseWidth,\
-             0.75 * baseWidth, 0.75 * baseWidth)
+ppL5.addRect(0.25*baseWidth, 0.125*baseWidth,0.75*baseWidth, 0.75*baseWidth)
 # set up ppR5 (right 5' blue square)
-ppR5.addRect(0, 0.125 * baseWidth,\
-             0.75 * baseWidth, 0.75 * baseWidth)
+ppR5.addRect(0, 0.125*baseWidth, 0.75*baseWidth, 0.75*baseWidth)
 # set up ppL3 (left 3' blue triangle)
 l3poly = QPolygonF()
 l3poly.append(QPointF(baseWidth, 0))
-l3poly.append(QPointF(0.25 * baseWidth, 0.5 * baseWidth))
+l3poly.append(QPointF(0.25*baseWidth, 0.5*baseWidth))
 l3poly.append(QPointF(baseWidth, baseWidth))
 ppL3.addPolygon(l3poly)
 # set up ppR3 (right 3' blue triangle)
 r3poly = QPolygonF()
 r3poly.append(QPointF(0, 0))
-r3poly.append(QPointF(0.75 * baseWidth, 0.5 * baseWidth))
+r3poly.append(QPointF(0.75*baseWidth, 0.5*baseWidth))
 r3poly.append(QPointF(0, baseWidth))
 ppR3.addPolygon(r3poly)
+# single base left 5'->3'
+pp53.addRect(0.25*baseWidth, 0.125*baseWidth, 0.25*baseWidth, 0.75*baseWidth)
+poly53 = QPolygonF()
+poly53.append(QPointF(0.5*baseWidth, 0))
+poly53.append(QPointF(baseWidth, 0.5*baseWidth))
+poly53.append(QPointF(0.5*baseWidth, baseWidth))
+poly53.addPolygon(poly53)
+# single base left 3'<-5'
+pp35.addRect(0.50*baseWidth, 0.125*baseWidth, 0.25*baseWidth, 0.75*baseWidth)
+poly35 = QPolygonF()
+poly35.append(QPointF(0.5*baseWidth, 0))
+poly35.append(QPointF(0, 0.5*baseWidth))
+poly35.append(QPointF(0.5*baseWidth, baseWidth))
+poly35.addPolygon(poly35)
+
 NoPen = QPen(Qt.NoPen)
 
 class NormalStrandGraphicsItem(QGraphicsLineItem):
