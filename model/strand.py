855de4ba2e7fe924211af8b39754eac24a71e983
grinner <a.grinner@gmail.com>
2011-09-20 18:26:55 -0400

    created strand.py skeleton

--- model/strand.py
+++ model/strand.py
@@ -24,3 +24,79 @@
 # THE SOFTWARE.
 #
 # http://www.opensource.org/licenses/mit-license.php
+
+
+import util
+
+# import Qt stuff into the module namespace with PySide, PyQt4 independence
+util.qtWrapImport('QtCore', globals(), ['pyqtSignal', 'QObject', 'Qt'])
+util.qtWrapImport('QtGui', globals(), ['QUndoStack'])
+
+class Strand(QObject):
+    
+    def __init__(self, vstrand, indexL, indexR):
+        super(Strand, self).__init__(vstrand)
+        self._oligo = None
+        self._vstrand = vstrand
+        
+        self._strand5p = None
+        self._strand3p = None
+        self.vBaseIndexL = indexL
+        self.vBaseIndexR = indexR
+        
+        
+        self._decorators = {}
+        self._sequence = None
+        self._note = None
+    # end def
+    
+    ### SIGNALS ###
+    hasNewOligoSignal = pyqtSignal(QObject)
+    destroyedSignal = pyqtSignal(QObject)
+    resizedSignal = pyqtSignal(QObject, int)
+    xover3pCreatedSignal = pyqtSignal(QObject, int)
+    xover3pDestroyedSignal = pyqtSignal(QObject, int)
+    decoratorCreatedSignal = pyqtSignal(QObject, QObject, int)
+    decoratorDestroySignal = pyqtSignal(QObject, int)
+    
+    ### SLOTS ###
+    
+    
+    ### Methods ###
+    def vstrand(self):
+        return self._vstrand
+    # end def
+    
+    def oligo(self):
+        return self._oligo
+    # end def
+    
+    def setNewOligo(self, newOligo):
+        
+    # end def
+    
+    def destroy(self):
+        
+    # end def
+    
+    def resize(self):
+    
+    # end def
+    
+    def merge(self, idx):
+        
+    # end def
+    
+    def split(self, idx):
+        
+    # end def
+    
+    def copy(self):
+        
+    # end def
+    
+    def addSequence(self):
+        
+    # end def
+    
+    
