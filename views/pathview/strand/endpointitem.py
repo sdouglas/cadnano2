29fdb740e238b7d5206a54e21c047ac971422393
grinner <a.grinner@gmail.com>
2011-09-29 22:09:36 -0400

    Updating the stranditem stuff to enable drawing

--- views/pathview/strand/endpointitem.py
+++ views/pathview/strand/endpointitem.py
@@ -31,11 +31,63 @@ import util
 util.qtWrapImport('QtCore', globals(), ['pyqtSignal', 'QObject'])
 util.qtWrapImport('QtGui', globals(), [])
 
-class EndpointItem(QGraphicsPathItem):
-    def __init__(self, parent):
-        """The parent should be a StrandItem."""
-        super(EndpointItem, self).__init__(parent)
+baseWidth = styles.PATH_BASE_WIDTH
+NoPen = QPen(Qt.NoPen)
+ppL5 = QPainterPath()  # Left 5' PainterPath
+ppR5 = QPainterPath()  # Right 5' PainterPath
+ppL3 = QPainterPath()  # Left 3' PainterPath
+ppR3 = QPainterPath()  # Right 3' PainterPath
+pp53 = QPainterPath()  # Left 5', Right 3' PainterPath
+pp35 = QPainterPath()  # Left 5', Right 3' PainterPath
+# set up ppL5 (left 5' blue square)
+ppL5.addRect(0.25*baseWidth, 0.125*baseWidth,0.75*baseWidth, 0.75*baseWidth)
+# set up ppR5 (right 5' blue square)
+ppR5.addRect(0, 0.125*baseWidth, 0.75*baseWidth, 0.75*baseWidth)
+# set up ppL3 (left 3' blue triangle)
+l3poly = QPolygonF()
+l3poly.append(QPointF(baseWidth, 0))
+l3poly.append(QPointF(0.25*baseWidth, 0.5*baseWidth))
+l3poly.append(QPointF(baseWidth, baseWidth))
+ppL3.addPolygon(l3poly)
+# set up ppR3 (right 3' blue triangle)
+r3poly = QPolygonF()
+r3poly.append(QPointF(0, 0))
+r3poly.append(QPointF(0.75*baseWidth, 0.5*baseWidth))
+r3poly.append(QPointF(0, baseWidth))
+ppR3.addPolygon(r3poly)
 
+
+# single base left 5'->3'
+pp53.addRect(0, 0.125*baseWidth, 0.5*baseWidth, 0.75*baseWidth)
+poly53 = QPolygonF()
+poly53.append(QPointF(0.5*baseWidth, 0))
+poly53.append(QPointF(baseWidth, 0.5*baseWidth))
+poly53.append(QPointF(0.5*baseWidth, baseWidth))
+pp53.addPolygon(poly53)
+# single base left 3'<-5'
+pp35.addRect(0.50*baseWidth, 0.125*baseWidth, 0.5*baseWidth, 0.75*baseWidth)
+poly35 = QPolygonF()
+poly35.append(QPointF(0.5*baseWidth, 0))
+poly35.append(QPointF(0, 0.5*baseWidth))
+poly35.append(QPointF(0.5*baseWidth, baseWidth))
+pp35.addPolygon(poly35)
+
+class EndpointItem(QGraphicsPathItem):
+    def __init__(self, strandItem, isDrawn5to3, captype):
+        """The parent should be a StrandItem."""
+        super(EndpointItem, self).__init__(strandItem)
+        self._strandItem = strandItem
+        self._is5prime = is5prime
+        self._isDrawn5to3 = isDrawn5to3
+        if captype = 'low':
+            path = ppL5 if drawn5To3 else ppL3
+        elif captype = 'high':
+            path = ppR3 if drawn5To3 else ppR5
+        else:
+            path = pp53 if drawn5To3 else pp35
+        self.setPath(path)
+    # end def
+        
     ### SIGNALS ###
 
     ### SLOTS ###
--- views/pathview/strand/stranditem.py
+++ views/pathview/strand/stranditem.py
@@ -26,15 +26,35 @@
 # http://www.opensource.org/licenses/mit-license.php
 
 from exceptions import NotImplementedError
+from controllers.itemcontrollers.strand.stranditemcontroller import StrandItemController
+from endpointitem import EndPointItem
+
 import util
 # import Qt stuff into the module namespace with PySide, PyQt4 independence
 util.qtWrapImport('QtCore', globals(), ['pyqtSignal', 'QObject'])
-util.qtWrapImport('QtGui', globals(), [])
+util.qtWrapImport('QtGui', globals(), ['QGraphicsLineItem', 'QGraphicsPathItem'])
 
-class StrandItem(QGraphicsPathItem):
-    def __init__(self, parent):
+class StrandItem(QGraphicsLineItem):
+    def __init__(self, modelStrand, virtualHelixItem):
         """The parent should be a VirtualHelixItem."""
-        super(StrandItem, self).__init__(parent)
+        super(StrandItem, self).__init__(virtualHelixItem)
+        self._modelStrand = modelStrand
+        self._virtualHelixItem = virtualHelixItem
+        isDrawn5To3 = modelStrand.drawn5To3()
+        lowCap = EndPointItem(self, 'low', isDrawn5To3)
+        lowCap.setPen(NoPen)
+        highCap = EndPointItem(self, 'high', isDrawn5To3)
+        highCap.setPen(NoPen)
+        dualCap = EndPointItem(self, 'dual', isDrawn5To3)
+        dualCap.setPen(NoPen)
+        
+        self._lowCap = lowCap
+        self._highCap = highCap
+        self._dualCap = dualCap
+        self._controller = StrandItemController(self, modelStrand)
+
+        self.update(modelStrand)
+    # end def
 
     ### SIGNALS ###
 
@@ -52,15 +72,86 @@ class StrandItem(QGraphicsPathItem):
         pass
 
     ### METHODS ###
-    def connectSignals(self):
-        self._strand.strandResizedSignal.connect(self.strandResizedSlot)
-        self._oligo.sequenceAddedSignal.connect(self.sequenceAddedSlot)
-        self._oligo.sequenceClearedSignal.connect(self.sequenceClearedSlot)
 
-    def disconnectSignals(self):
-        """docstring for disconnectSignals"""
-        self._strand.strandResizedSignal.disconnect(self.strandResizedSlot)
-        self._oligo.sequenceAddedSignal.disconnect(self.sequenceAddedSlot)
-        self._oligo.sequenceClearedSignal.disconnect(self.sequenceClearedSlot)
+
+    def update(self, strand):
+        """
+        Prepare NormalStrand for drawing:
+        1. Show or hide caps depending on L and R connectivity.
+        2. Determine line coordinates.
+        3. Apply paint styles.
+        """
+        # 0. Setup
+        vhi = self._virtualHelixItem
+        halfBaseWidth = vhi.baseWidth / 2.0
+        lowIdx, highIdx = strand.lowIdx(), strand.highIdx()
+        
+        lUpperLeftX, lUpperLeftY = vhi.upperLeftCornerOfBase(lowIdx)
+        hUpperLeftX, hUpperLeftY = vhi.upperLeftCornerOfBase(highIdx)
+        
+        lowCap = self._lowCap
+        highCap = self._highCap
+        dualCap = self._dualCap
+        
+        # 1. Cap visibilty
+        if strand.apparentlyConnectedL():  # hide left cap if L-connected
+            lx = lUpperLeftX
+            leftCap.hide()
+        else:  # otherwise show left cap
+            lx = lUpperLeftX + halfBaseWidth
+            leftCap.setPos(lUpperLeftX, lUpperLeftY)
+            leftCap.show()
+        if strand.apparentlyConnectedR():  # hide right cap if R-connected
+            hx = rUpperLeftX + vhi.baseWidth
+            highCap.hide()
+        else:  # otherwise show it
+            hx = rUpperLeftX + halfBaseWidth
+            highCap.setPos(rUpperLeftX, rUpperLeftY)
+            highCap.show()
+        # special case: single-base strand with no L or R connections,
+        # (unconnected caps were made visible in previous block of code)
+        if strand.numBases() == 1 and \
+                  (self.leftCap.isVisible() and self.rightCap.isVisible()):
+            lowCap.hide()  # hide 
+            highCap.hide()
+            dualCap.setPos(lUpperLeftX, lUpperLeftY)
+            dualCap.show()
+        else:
+            dualCap.hide()
+        # 2. Line drawing
+        hy = ly = lUpperLeftY + halfBaseWidth
+        self.setLine(lx, ly, hx, hy)
+        self.updatePensAndBrushes(strand)
+
+    def updatePensAndBrushes(self, strand):
+        lowIdx, highIdx = strand.lowIdx(), strand.highIdx()
+        
+        if strand.isScaf():
+            pen = QPen(styles.scafstroke, styles.PATH_STRAND_STROKE_WIDTH)
+            brush = QBrush(styles.handlefill)
+        else:
+            colr = self._modelStrand.oligo().color()
+            pen = QPen(colr, styles.PATH_STRAND_STROKE_WIDTH)
+            brush = QBrush(colr)
+        pen.setCapStyle(Qt.FlatCap)
+        self.setPen(pen)
+        self._lowCap.setBrush(brush)
+        self._highCap.setBrush(brush)
+        self._dualCap.setBrush(brush)
+
+    def remove(self, strand):
+        ns = self.normalStrand
+        ns.didMove.disconnect(self.update)
+        ns.apparentConnectivityChanged.disconnect(self.update)
+        ns.willBeRemoved.disconnect(self.remove)
+        scene = self.scene()
+        scene.removeItem(self.rightCap)
+        self.scene().removeItem(self.leftCap)
+        self.rightCap = None
+        self.leftCap = None
+        scene.removeItem(self)
+    # end def
+    
 
     ### COMMANDS ###
+    
