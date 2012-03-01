d25fc235d77132f138690e5bb4bbf558c030d41a
grinner <a.grinner@gmail.com>
2012-02-29 21:28:05 -0500

    did some forced xover visibility tweaks

--- views/pathview/strand/endpointitem.py
+++ views/pathview/strand/endpointitem.py
@@ -319,6 +319,8 @@ class EndpointItem(QGraphicsPathItem):
         if not activeTool.isFloatingXoverBegin():
             tempXover = activeTool.floatingXover()
             tempXover.updateFloatingFromStrandItem(vhi, mStrand, idx)
+            if mStrand.idx5Prime() == idx:
+                tempXover.hide3prime()
     # end def
 
     def pencilToolMousePress(self, modifiers, event, idx):
@@ -329,9 +331,12 @@ class EndpointItem(QGraphicsPathItem):
         activeTool = self._activeTool()
 
         if activeTool.isFloatingXoverBegin():
+            if mStrand.idx5Prime() == idx:
+                return
             tempXover = activeTool.floatingXover()
             tempXover.updateBase(vhi, mStrand, idx)
             activeTool.setFloatingXoverBegin(False)
+            tempXover.hide5prime()
         else:
             activeTool.setFloatingXoverBegin(True)
             # install Xover
